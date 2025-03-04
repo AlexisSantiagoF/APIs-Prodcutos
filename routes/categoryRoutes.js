01a0645e32dfe74f88aa48423e54bc4d202f84d1
ReacherDaniel <105395995+ReacherDaniel@users.noreply.github.com>
2025-03-04 15:02:53 -0700

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 
+// Definir rutas
 router.get('/', categoryController.getAllCategories);
-router.post('/', categoryController.createCategory);
+router.get('/:id', categoryController.getCategoryById);
 
 module.exports = router;
