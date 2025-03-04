26daf4cd155dfab136a99f3ed9740bbe519d10f2
ReacherDaniel <105395995+ReacherDaniel@users.noreply.github.com>
2025-03-04 15:02:26 -0700

    Update  app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+
+// Importar rutas correctamente
 const productRoutes = require('./routers/productRoutes');
+const categoryRoutes = require('./routers/categoryRoutes');
 
 app.use(express.json());
 
+// Definir rutas
 app.use('/apiV1/products', productRoutes);
+app.use('/apiV1/categories', categoryRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
