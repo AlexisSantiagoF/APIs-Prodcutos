18267437880c66446c2ed84c47b67c70e871fa69
MiguelLopezRamirez <miguelangellr14@gmail.com>
2025-03-13 08:27:47 -0700

    Adding public folder

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 // Importar rutas correctamente
 const productRoutes = require('./routes/productRoutes');
@@ -13,6 +14,10 @@ app.use('/apiV1/products', productRoutes);
 app.use('/apiV1/categories', categoryRoutes);
 app.use('/apiV1/purchases', purchasesRoutes)
 app.use('/apiV1/users', userRoutes);
+
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static(path.join(__dirname, "public")));
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
